e1e920a4b84a255ec2c4ebaac14d1abe82fd3010
100plans <hundredplans@gmail.com>
2026-03-24 18:42:06 +0100

    forgot

--- project/scripts/actions/start_turn_action.gd
+++ project/scripts/actions/start_turn_action.gd
@@ -5,7 +5,10 @@ func _init(_players: bool) -> void:
 	players = _players
 	
 func onPreAction() -> void: pass
-func onPostAction() -> void: pass
+func onPostAction() -> void:
+	if Board.getCharacter(players).getHandSize() < Data.MIN_HAND_SIZE:
+		onPush([DrawCardAction.new(1, players)])
+	
 func isPlayers() -> bool: return players
 func getLogInfo() -> Array:
 	return ["Players: %s" % players]
